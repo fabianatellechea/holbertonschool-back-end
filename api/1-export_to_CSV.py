5f110641e4d2c24030ac5b71e702264e206e2267
Fabiana Tellechea <85513647+fabianatellechea@users.noreply.github.com>
2023-12-19 22:34:44 +0000

    added: task 1

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""script to export data in the CSV format. """
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    todos = requests.get(
+        f'https://jsonplaceholder.typicode.com/todos?userId={argv[1]}')
+    user = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
+    USERNAME = user.json().get('username')
+
+with open(f'{argv[1]}.csv', 'w') as f:
+    for task in todos.json():
+        TASK_COMPLETED_STATUS = task.get('completed')
+        TASK_TITLE = task.get('title')
+        f.write('"{}","{}","{}","{}"\n'.
+                format(argv[1], USERNAME, TASK_COMPLETED_STATUS, TASK_TITLE))
